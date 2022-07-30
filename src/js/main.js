b28579452033d0bf7fef78e0d7e22fbae32cee2e
moisesrp2021 <46361656+master-hit@users.noreply.github.com>
2022-07-30 18:00:39 -0400

    attempt00: THREE MODE
    grouping functions by functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,21 @@
-
-// import {globalShortcut} from 'electron';
-
+(function() {
+     const electron = require('electron');
+})
 document.addEventListener ("keydown", (event) => {
-	event.preventDefault();
-	manageKeys(event);
+     event.preventDefault();
+     manageKeys(event);
 });
 
 function manageKeys(event) {
-	console.log(event.key);
-	switch(event.key) {
-	case "j":
-		move_cursor_down();
-		break;
-	case "k":
-		move_cursor_up();
-		break;		
-	case "l":
-		move_cursor_left();
-		break;		
-	case "h":
-		move_cursor_right();
-		break;		
-	case " ":
-		console.log(buffer[index_y][index_x]);
-		break;		
-	case "i":
-		change_mode(MODE.INSERT);
-		break;		
-	case "Escape":
-		change_mode(MODE.NORMAL);
-		break;		
-	}
+     switch(current_mode) {
+          case MODE.INSERT:
+               // manageKeyInsertMode(event);
+               break;
+          case MODE.NORMAL:
+               manageKeyNormalMode(event);
+               break;
+          case MODE.VISUAL:
+               // manageKeyVisualMode(event);
+               break;
+     }
 }
